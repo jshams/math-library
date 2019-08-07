4fca8beec8847c75465eb40fefb9a946e7ea3674
Jake Shams <jshams62@gmail.com>
2019-08-06 17:52:47 -0700

    index file

---
+++ index.js
@@ -0,0 +1,20 @@
+Number.prototype.round = function round() {
+    return Math.round(this);
+};
+
+Number.prototype.floor = function floor() {
+    return Math.floor(this);
+};
+
+Number.prototype.ceil = function ceil() {
+    return Math.ceil(this);
+};
+
+Number.prototype.split = function split(n) {
+    let arr = Array(n).fill(Math.floor((100 * this) / n) / 100)
+    const remainder = (100 * this) % n
+    for (let i = 0; i < remainder; i++) {
+        arr[i] = arr[i] + 0.01
+    }
+    return arr
+}
